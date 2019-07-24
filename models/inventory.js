5a206bcee197b1b8292b72da4a3fe8ebcd8ebecc
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-24 10:58:10 -0500

    changed the order of columns so that car_id is at the end instead of the front

--- models/inventory.js
+++ models/inventory.js
@@ -1,8 +1,8 @@
 module.exports = function (sequelize, DataTypes) {
     var inventory = sequelize.define("inventory", {
-        car_id = DataTypes.INTEGER,
         milesDriven: DataTypes.INTEGER,
         available: DataTypes.BOOLEAN,
+        car_id = DataTypes.INTEGER
     }, {
             freezeTableName: true
         })
