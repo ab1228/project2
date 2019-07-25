a32065c70bb1fb940fac0bc46f13d8b50eeb229b
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-25 13:36:05 -0500

    added email column to users table

--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false
         },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         checkout: DataTypes.DATE,
         checkin: DataTypes.DATE,
         inventory_id: DataTypes.INTEGER,
