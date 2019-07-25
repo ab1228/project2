08cbccf45a7154205ad450520cfc5b84394400e2
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-25 10:58:21 -0500

    removed unnecessary fields in the car model.  Added inventory model and user model.  Started work on seeds

--- models/car.js
+++ models/car.js
@@ -3,19 +3,9 @@ module.exports = function(sequelize, DataTypes){
        make: DataTypes.STRING,
        model: DataTypes.STRING,
        year: DataTypes.INTEGER,
-       engineFuelType: DataTypes.STRING,
-       engineHP: DataTypes.INTEGER,
-       engineCylinders: DataTypes.INTEGER,
-       transmissionType: DataTypes.STRING,
-       drivenWheels: DataTypes.STRING,
-       numberOfDoors: DataTypes.INTEGER,
        marketCategory: DataTypes.STRING,
        vehicleSize: DataTypes.STRING,
-       vehicleStyle: DataTypes.STRING,
-       highwayMPG: DataTypes.INTEGER,
-       cityMPG: DataTypes.INTEGER,
-       popularity: DataTypes.INTEGER,
-       msrp: DataTypes.INTEGER
+       vehicleStyle: DataTypes.STRING
     })
 
     return car;
