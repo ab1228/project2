b98de098f8fbdb6706c0064906c8755576dbd207
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-25 11:55:19 -0500

    created seed for inventory to be put into mySQL

---
+++ db/seeders/inventorySeed.sql
@@ -0,0 +1,54 @@
+SELECT *
+FROM cars_db.cars;
+
+INSERT INTO inventory
+    (milesDriven, available, car_id, createdAt, updatedAt)
+VALUES
+    (1204, true, 620, current_time
+(), current_time
+()),
+(725, true, 640, current_time
+(), current_time
+()),
+(500, true, 767, current_time
+(), current_time
+()),
+(944, true, 944, current_time
+(), current_time
+()),
+(75, true, 957, current_time
+(), current_time
+()),
+(4500, true, 972, current_time
+(), current_time
+()),
+(1242, true, 949, current_time
+(), current_time
+()),
+(1443, true, 905, current_time
+(), current_time
+()),
+(4000, true, 620, current_time
+(), current_time
+()),
+(10323, true, 767, current_time
+(), current_time
+()),
+(653, true, 818, current_time
+(), current_time
+()),
+(2043, true, 805, current_time
+(), current_time
+()),
+(3254, true, 787, current_time
+(), current_time
+()),
+(12456, true, 944, current_time
+(), current_time
+()),
+(5432, true, 702, current_time
+(), current_time
+()),
+(9843, true, 707, current_time
+(), current_time
+())
