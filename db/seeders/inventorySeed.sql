71340b9da3dcc3dd86a93bb4f5d0e8a3b5f5da90
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-25 21:25:09 -0500

    new seed for inventory

--- db/seeders/inventorySeed.sql
+++ db/seeders/inventorySeed.sql
@@ -2,53 +2,17 @@ SELECT *
 FROM cars_db.cars;
 
 INSERT INTO inventory
-    (milesDriven, available, car_id, createdAt, updatedAt)
+    (make, model, available, createdAt, updatedAt)
 VALUES
-    (1204, true, 620, current_time
-(), current_time
-()),
-(725, true, 640, current_time
-(), current_time
-()),
-(500, true, 767, current_time
-(), current_time
-()),
-(944, true, 944, current_time
-(), current_time
-()),
-(75, true, 957, current_time
-(), current_time
-()),
-(4500, true, 972, current_time
-(), current_time
-()),
-(1242, true, 949, current_time
-(), current_time
-()),
-(1443, true, 905, current_time
-(), current_time
-()),
-(4000, true, 620, current_time
-(), current_time
-()),
-(10323, true, 767, current_time
-(), current_time
-()),
-(653, true, 818, current_time
-(), current_time
-()),
-(2043, true, 805, current_time
-(), current_time
-()),
-(3254, true, 787, current_time
-(), current_time
-()),
-(12456, true, 944, current_time
-(), current_time
-()),
-(5432, true, 702, current_time
-(), current_time
-()),
-(9843, true, 707, current_time
-(), current_time
-())
+    ("BMW", "Z4", true, current_time(), current_time()),
+    ("Audi", "A8", true, current_time(), current_time()),
+    ("Mercedes-Benz", "S 450", true, current_time(), current_time()),
+    ("Lexus", "GS 350", true, current_time(), current_time()),
+    ("Nissan", "Q70", true, current_time(), current_time()),
+    ("Audi", "A6", true, current_time(), current_time()),
+    ("Audi", "A8", true, current_time(), current_time()),
+    ("Mercedes-Benz", "E 300", true, current_time(), current_time()),
+    ("Lincoln", "MKZ", true, current_time(), current_time()),
+    ("Cadillac", "Escalade", true, current_time(), current_time()),
+    ("BMW", "X5", true, current_time(), current_time()),
+    ("Cadillac", "CTS", true, current_time(), current_time())
