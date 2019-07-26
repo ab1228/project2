e30b3f7f2f5a426006a1d8ddf1f1974661b322dd
Deepali Singh <14singh.deepali@gmail.com>
2019-07-26 09:46:23 -0500

    passport

--- models/user.js
+++ models/user.js
@@ -10,6 +10,10 @@ module.exports = function (sequelize, DataTypes) {
         password: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
     })
     return user;
