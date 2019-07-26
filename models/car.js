326cc02d46630dab90a37454808c903a5a6da7b8
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-25 21:01:32 -0500

    commented out as we shouldn't need this table any more

--- models/car.js
+++ models/car.js
@@ -1,18 +1,22 @@
-module.exports = function(sequelize, DataTypes){
-    var Car = sequelize.define("Car",{
-       make: DataTypes.STRING,
-       model: DataTypes.STRING,
-       year: DataTypes.INTEGER,
-       marketCategory: DataTypes.STRING,
-       vehicleSize: DataTypes.STRING,
-       vehicleStyle: DataTypes.STRING
-    });
+// ////////////////////////////
+// Unnecessary for our project
+// ///////////////////////////
 
-    Car.associate = function(models) {
-        Car.hasMany(models.Inventory,{
-            onDelete: "cascade"
-        });
-    };
+// module.exports = function(sequelize, DataTypes){
+//     var Car = sequelize.define("Car",{
+//        make: DataTypes.STRING,
+//        model: DataTypes.STRING,
+//        year: DataTypes.INTEGER,
+//        marketCategory: DataTypes.STRING,
+//        vehicleSize: DataTypes.STRING,
+//        vehicleStyle: DataTypes.STRING
+//     });
+
+//     Car.associate = function(models) {
+//         Car.hasMany(models.Inventory,{
+//             onDelete: "cascade"
+//         });
+//     };
     
-    return Car;
-}
+//     return Car;
+// }
