fc9dad28ce97d9b2c043e016a43dd77655b4c84a
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-28 23:47:17 -0500

    created the ajax call to delete a user and then reload the page

--- public/assets/js/deleteUser.js
+++ public/assets/js/deleteUser.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
         event.preventDefault();
         var user_id = $(this).children(".user_id").val();
         console.log(user_id);
+
+        $.ajax({
+            method: "DELETE",
+            url: "/api/users/" + user_id
+        })
+        .then(function(){
+            location.reload();
+        })
     })
 })
