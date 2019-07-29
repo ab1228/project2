cc79ca2da978c5e7e5dc06b502751b85cdc0b9b0
Deepali Singh <14singh.deepali@gmail.com>
2019-07-29 14:37:17 -0500

    passport sign-in/login-in

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     // Getting references to our form and inputs
-    var loginForm = $("form.login");
-    var emailInput = $("input#email-input");
-    var passwordInput = $("input#password-input");
+    var loginForm = $("#login");
+    var emailInput = $("#email");
+    var passwordInput = $("#userPassword");
 
     // When the form is submitted, we validate there's an email and password entered
-    loginForm.on("submit", function (event) {
+    loginForm.on("click", function (event) {
+        alert("clicked")
         event.preventDefault();
         var userData = {
             email: emailInput.val().trim(),
@@ -28,12 +29,6 @@ $(document).ready(function () {
             email: email,
             password: password
         })
-            .then(function () {
-                window.location.replace("/members");
-                // If there's an error, log the error
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
+
     }
 });
--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,38 +1,42 @@
 $(document).ready(function () {
     // Getting references to our form and input
-    var signUpForm = $("form.signup");
-    var emailInput = $("input#email-input");
-    var passwordInput = $("input#password-input");
+    var signUpForm = $("#sign-up");
 
     // When the signup button is clicked, we validate the email and password are not blank
-    signUpForm.on("submit", function (event) {
+    signUpForm.on("click", function (event) {
+        var emailInput = $("#email");
+        var passwordInput = $("#password");
+        var name = $("#name");
+
         event.preventDefault();
         var userData = {
-            email: emailInput.val().trim(),
-            password: passwordInput.val().trim()
+            name: name.val(),
+            email: emailInput.val(),
+            password: passwordInput.val()
         };
+        console.log(userData);
 
-        if (!userData.email || !userData.password) {
+        if (!userData.email || !userData.password || !userData.name) {
+            // alert("complete form")
             return;
         }
         // If we have an email and password, run the signUpUser function
-        signUpUser(userData.email, userData.password);
+        signUpUser(userData);
         emailInput.val("");
         passwordInput.val("");
     });
 
     // Does a post to the signup route. If successful, we are redirected to the members page
     // Otherwise we log any errors
-    function signUpUser(email, password) {
-        $.post("/api/signup", {
-            email: email,
-            password: password
+    function signUpUser(userData) {
+        $.post("/api/signup", userData, function (data) {
+
         })
-            .then(function (data) {
-                window.location.replace("/members");
-                // If there's an error, handle it by throwing up a bootstrap alert
-            })
-            .catch(handleLoginErr);
+        // .then(function (data) {
+        //     window.location.replace("/members");
+        //     // If there's an error, handle it by throwing up a bootstrap alert
+        // })
+
     }
 
     function handleLoginErr(err) {
